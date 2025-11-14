1b133758be4135cf01466883dfeb99cc499cfa33
ginoblade <ginoblade@gmail.com>
2025-11-14 19:36:15 +0800

    Refactor authentication controller and update token expiration to 30 days

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -7,33 +7,29 @@ import { generateToken } from "../lib/utils";
 export const signup = async (req: Request, res: Response) => {
   const { name, email, password, avatar } = req.body;
   try {
-    // if (password.length < 8) {
-    //   return res
-    //     .status(400)
-    //     .json({ message: "Password must be at least 8 characters" });
-    // }
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
 
-    // if (password.length < 8) {
-    //   return res
-    //     .status(400)
-    //     .json({ message: "Password must be at least 8 characters" });
-    // }
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 8 characters." });
+    }
 
     const user = await User.findOne({ email });
     if (user) {
-      return res.status(400).json({ message: "Email already exist" });
+      return res.status(400).json({ message: "Email already exist." });
     }
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
-
     const newUser = new User({
       name: name,
       email: email,
       password: hash,
       avatar: avatar,
     });
-
     if (newUser) {
       generateToken(newUser._id, res);
       await newUser.save();
@@ -45,24 +41,58 @@ export const signup = async (req: Request, res: Response) => {
         avatar: newUser.avatar,
       });
     } else {
-      res.status(400).json({ message: "Invalid user data" });
+      res.status(400).json({ message: "Invalid user data." });
     }
   } catch (error) {
-    let errorMessage = "";
-    if (error instanceof Error) {
-      errorMessage = "Signup error: " + error.message;
-    } else {
-      errorMessage = "An unknown error occurred.";
-    }
-    console.log(errorMessage);
-    res.status(500).json({ message: errorMessage });
+    res.status(500).json({
+      message:
+        error instanceof Error
+          ? "Signup error: " + error.message
+          : "An unknown error occurred.",
+    });
   }
 };
 
-export const login = (_req: Request, res: Response) => {
-  res.send("login route");
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message:
+        error instanceof Error
+          ? "Signup error: " + error.message
+          : "An unknown error occurred.",
+    });
+  }
 };
 
-export const logout = (_req: Request, res: Response) => {
-  res.send("logout route");
+export const logout = (req: Request, res: Response) => {
+  try {
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logged out successfully." });
+  } catch (error) {
+    res.status(500).json({
+      message:
+        error instanceof Error
+          ? "Signup error: " + error.message
+          : "An unknown error occurred.",
+    });
+  }
 };
--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -5,11 +5,11 @@ import { Types } from "mongoose";
 
 export const generateToken = (userId: Types.ObjectId, res: Response) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET || "", {
-    expiresIn: "1d",
+    expiresIn: "30d",
   });
 
-  res.cookie("jwt", token, {
-    maxAge: 1 * 24 * 60 * 60 * 1000,
+  res.cookie("token", token, {
+    maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
     secure: process.env.NODE_ENV !== "dev",
