999484ba0397ce5aeae48a4ff4f9383fc5a11f77
ginoblade <ginoblade@gmail.com>
2025-11-19 09:00:27 +0800

    feat: implement SignUp component with form handling and validation; add SignUpData type

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,9 +1,126 @@
-import React from 'react'
+import { useState } from "react";
+import { Mail, MessageSquare, User, Lock, EyeOff, Eye } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
+import type { SignUpData } from "../types/SignUpData";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState<boolean>();
+  // const [formData, setFormData] = useState<FormData>();
+  const [formData, setFormData] = useState<SignUpData>({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-export default SignUp
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {
+    // Add form validation logic here
+    return true;
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // if (!validateForm()) return;
+    // await signup(formData);
+  };
+
+  return (
+    //[TODO] Change forms to use daisyUI components
+    <div className="min-h-screen grid lg:grid-cols-2">
+      <div className="flex flex-col justify-center items-center p-8 sm:p-16">
+        <div className="w-full max-w-md space-y-8">
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div
+                className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+              group-hover:bg-primary/20 transition-colors"
+              >
+                <MessageSquare className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+              <p className="text-base-content/60">
+                Get started with your free account
+              </p>
+            </div>
+          </div>
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="form-control ">
+              <label className="label">
+                <span className="label-text font-medium">Full Name</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
+                  <User className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="John Doe"
+                  value={formData.name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
+                  <Mail className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="email"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@example.com"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
+                  <Lock className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="••••••••"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40 z-10" />
+                  ) : (
+                    <Eye className="size-5 text-base-content/40 z-10" />
+                  )}
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
--- client/src/store/useAuthStore.ts
+++ client/src/store/useAuthStore.ts
@@ -1,14 +1,21 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import type { SignUpData } from "../types/SignUpData";
 
-export const useAuthStore = create<{
+type LoginData = Pick<SignUpData, "email" | "password">;
+
+interface AuthState {
   isAuthenticated: null | boolean;
-  checkAuth: () => Promise<void>;
   isSigningUp: boolean;
   isLoggingIn: boolean;
   isUpdatingProfile: boolean;
   isCheckingAuth: boolean;
-}>((set) => ({
+  checkAuth: () => Promise<void>;
+  signup: (data: SignUpData) => Promise<void>;
+//   login: (data: LoginData) => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>()((set) => ({
   isAuthenticated: null,
   isSigningUp: false,
   isLoggingIn: false,
@@ -25,4 +32,28 @@ export const useAuthStore = create<{
       set({ isCheckingAuth: false });
     }
   },
+
+  signup: async (data: SignUpData) => {
+    set({ isSigningUp: true });
+    try {
+      await axiosInstance.post("/auth/signup", data);
+      set({ isAuthenticated: true });
+    } catch (e) {
+      set({ isAuthenticated: false });
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+//   login: async (data: LoginData) => {
+//     set({ isLoggingIn: true });
+//     try {
+//       await axiosInstance.post("/auth/login", data);
+//       set({ isAuthenticated: true });
+//     } catch (e) {
+//       set({ isAuthenticated: false });
+//     } finally {
+//       set({ isLoggingIn: false });
+//     }
+//   },
 }));
