5c1b3825e09f2201cde1f386b366b231de3d11e1
ginoblade <ginoblade@gmail.com>
2025-11-19 23:01:01 +0800

    feat: add lucide-react dependency and update SignUp component with enhanced form validation

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import { Mail, MessageSquare, User, Lock, EyeOff, Eye } from "lucide-react";
+import { Eye, EyeOff, KeyRound, Mail, MessageSquare, User } from "lucide-react";
 import { useAuthStore } from "../store/useAuthStore";
 import type { SignUpData } from "../types/SignUpData";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState<boolean>();
-  // const [formData, setFormData] = useState<FormData>();
-  const [formData, setFormData] = useState<SignUpData>({
+  const [signUpData, setSignUpData] = useState<SignUpData>({
     name: "",
     email: "",
     password: "",
@@ -26,7 +25,6 @@ const SignUp = () => {
   };
 
   return (
-    //[TODO] Change forms to use daisyUI components
     <div className="min-h-screen grid lg:grid-cols-2">
       <div className="flex flex-col justify-center items-center p-8 sm:p-16">
         <div className="w-full max-w-md space-y-8">
@@ -43,80 +41,95 @@ const SignUp = () => {
                 Get started with your free account
               </p>
             </div>
+
+            <form onSubmit={handleSubmit} className="py-12">
+              <div>
+                <label className="input validator">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <User className="size-5 text-base-content/40" />
+                  </div>
+                  <input
+                    type="text"
+                    className="pl-8"
+                    required
+                    placeholder="Username"
+                    pattern="[A-Za-z][A-Za-z0-9\-]*"
+                    minLength={3}
+                    maxLength={30}
+                    title="Only letters, numbers or dash"
+                    value={signUpData.name}
+                    onChange={(e) =>
+                      setSignUpData({ ...signUpData, name: e.target.value })
+                    }
+                  />
+                </label>
+                <p className="validator-hint hidden">
+                  Must be 3 to 30 characters
+                  <br />
+                  containing only letters, numbers or dash
+                </p>
+              </div>
+              <div className="py-3">
+                <label className="input validator">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <Mail className="size-5 text-base-content/40" />
+                  </div>
+                  <input
+                    type="email"
+                    className="pl-8"
+                    placeholder="mail@site.com"
+                    required
+                    value={signUpData.email}
+                    onChange={(e) =>
+                      setSignUpData({ ...signUpData, email: e.target.value })
+                    }
+                  />
+                </label>
+                <p className="validator-hint hidden">
+                  Enter valid email address
+                </p>
+              </div>
+              <div>
+                <label className="input validator">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <KeyRound className="size-5 text-base-content/40" />
+                  </div>
+
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    className="pl-8"
+                    required
+                    placeholder="Password"
+                    minLength={8}
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                    value={signUpData.password}
+                    onChange={(e) =>
+                      setSignUpData({ ...signUpData, password: e.target.value })
+                    }
+                  />
+                  <button
+                    type="button"
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? (
+                      <EyeOff className="size-5 text-base-content/40" />
+                    ) : (
+                      <Eye className="size-5 text-base-content/40" />
+                    )}
+                  </button>
+                </label>
+                <p className="validator-hint hidden">
+                  Must be more than 8 characters, including
+                  <br />
+                  At least one number <br />
+                  At least one lowercase letter <br />
+                  At least one uppercase letter
+                </p>
+              </div>
+            </form>
           </div>
-
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="form-control ">
-              <label className="label">
-                <span className="label-text font-medium">Full Name</span>
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
-                  <User className="size-5 text-base-content/40" />
-                </div>
-                <input
-                  type="text"
-                  className={`input input-bordered w-full pl-10`}
-                  placeholder="John Doe"
-                  value={formData.name}
-                  onChange={(e) =>
-                    setFormData({ ...formData, name: e.target.value })
-                  }
-                />
-              </div>
-            </div>
-
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text font-medium">Email</span>
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
-                  <Mail className="size-5 text-base-content/40" />
-                </div>
-                <input
-                  type="email"
-                  className={`input input-bordered w-full pl-10`}
-                  placeholder="you@example.com"
-                  value={formData.email}
-                  onChange={(e) =>
-                    setFormData({ ...formData, email: e.target.value })
-                  }
-                />
-              </div>
-            </div>
-
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text font-medium">Password</span>
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
-                  <Lock className="size-5 text-base-content/40" />
-                </div>
-                <input
-                  type={showPassword ? "text" : "password"}
-                  className={`input input-bordered w-full pl-10`}
-                  placeholder="••••••••"
-                  value={formData.password}
-                  onChange={(e) =>
-                    setFormData({ ...formData, password: e.target.value })
-                  }
-                />
-                <button
-                  type="button"
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? (
-                    <EyeOff className="size-5 text-base-content/40 z-10" />
-                  ) : (
-                    <Eye className="size-5 text-base-content/40 z-10" />
-                  )}
-                </button>
-              </div>
-            </div>
-          </form>
         </div>
       </div>
     </div>
